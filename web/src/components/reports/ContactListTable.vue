cb4b0b375bb571be8a19be03a0a9951316246122
Adriano Oliveira <adrianosouza11@gmail.com>
2025-10-25 18:28:06 -0300

    Rename title button expoprtar csv to exportar para csv

--- web/src/components/reports/ContactListTable.vue
+++ web/src/components/reports/ContactListTable.vue
@@ -14,7 +14,7 @@
             @click="onDownloadReport"  
             class="bg-blue-500 text-white px-4 py-2 hover:bg-blue-600 rounded-full">
             <svg class="w-5 h-5 mr-2 -ml-1 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 0115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10"></path></svg>
-            Exportar CSV
+            Exportar para CSV
           </button>
 
         </div>
